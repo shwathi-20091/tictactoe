ad0c9632e8c789421eac1bc26487eceea05edc79
shwathi-20091 <103167133+shwathi-20091@users.noreply.github.com>
2022-04-27 14:04:20 +0530

    Update Evaluation.java

--- src/com/example/Evaluation.java
+++ src/com/example/Evaluation.java
@@ -6,9 +6,8 @@ package com.example;
  * This enumeration specifies the possible results of the evaluation of a Tic-Tac-Toe board.
  */
 public enum Evaluation {
-  NoWinner, /* valid board, but the game hasn't been won by either player yet */
-  Xwins, /* valid board, and the game was won by player X */
-  Owins, /* valid board, and the game was won by player O */
-  UnreachableState, /* string describes a board, but not a board state that is reachable from a valid game */
-  InvalidInput /*invalid board*/
+  noWinner, /* valid board, but the game hasn't been won by either player yet */
+  xwins, /* valid board, and the game was won by player X */
+  owins,invalidInput, /* valid board, and the game was won by player O */
+  unreachableState /* string describes a board, but not a board state that is reachable from a valid game */
 }
